572809e10914008f8ce377b08fc9b5307a5949e7
loej <jfm203@scarletmail.rutgers.edu>
2020-05-26 02:39:03 -0400

    Initial Commit.

---
+++ Complete Python Basics/basics-HelloWorld.py
@@ -0,0 +1,16 @@
+#[Automate the Boring Stuff with Python - Al Sweigart](http://automatetheboringstuff.com/)
+# Under the same creative commons license: https://creativecommons.org/licenses/by-nc-sa/3.0/
+
+#pritns 'Hello World'.
+print('Hello World')
+#Asks for any name and outputs any name.
+print('What is your name?')
+x = input()
+print('Nice to meet you ' + x + '.')
+#Prints length of name
+print('The length of your name is:')
+print(len(x))
+#Asks a for age
+g = input('What is your age? \n')
+print('Your age is: ' + g + '.')
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# Python: Basics & Data Structures 
+The following programs cover the basics of Python, from 'Hello World' to Data Structures.
+The program is  based off of  various online tutorials and books. I will keep updating 
+the list of books and authors. Licensed under a Creative Commons licence.
+# Books and Authors based off
+[Automate the Boring Stuff with Python - Al Sweigart](http://automatetheboringstuff.com/)
