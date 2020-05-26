e5b7149872494ad35afb526120912cff21439847
loej <jfm203@scarletmail.rutgers.edu>
2020-05-26 03:28:27 -0400

    Initial Commit - statements.py

---
+++ Complete Python Basics/statements.py
@@ -0,0 +1,3 @@
+#[Automate the Boring Stuff with Python - Al Sweigart](http://automatetheboringstuff.com/)
+# Under the same creative commons license: https://creativecommons.org/licenses/by-nc-sa/3.0/
+
