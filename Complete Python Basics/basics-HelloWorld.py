db5952b0f41e78bfae70e3e39fb2f174466b7668
loej <jfm203@scarletmail.rutgers.edu>
2020-05-26 03:19:54 -0400

    Finished conditionals.py

--- Complete Python Basics/basics-HelloWorld.py
+++ Complete Python Basics/basics-HelloWorld.py
@@ -1,16 +1,21 @@
 #[Automate the Boring Stuff with Python - Al Sweigart](http://automatetheboringstuff.com/)
 # Under the same creative commons license: https://creativecommons.org/licenses/by-nc-sa/3.0/
 
-#pritns 'Hello World'.
+# Prints 'Hello World'.
 print('Hello World')
-#Asks for any name and outputs any name.
+
+# Asks for any name and outputs any name.
 print('What is your name?')
 x = input()
 print('Nice to meet you ' + x + '.')
-#Prints length of name
+
+# Prints length of name
 print('The length of your name is:')
 print(len(x))
-#Asks a for age
+
+# Asks a for age and calculates the age a year later.
 g = input('What is your age? \n')
 print('Your age is: ' + g + '.')
+print('You will be: ' + str(int(g)+1) + ' in one year.')
 
+#
