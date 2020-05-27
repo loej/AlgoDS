9d0a0d64984b1482f4e5b2b1ff61b07065c70f36
loej <jfm203@scarletmail.rutgers.edu>
2020-05-27 19:35:58 -0400

    Final Changes to if-ifelse...py file

--- Complete Python Basics/if-ifelse-statements.py
+++ Complete Python Basics/if-ifelse-statements.py
@@ -1,22 +1,19 @@
 #[Automate the Boring Stuff with Python - Al Sweigart](http://automatetheboringstuff.com/)
 # Under the same creative commons license: https://creativecommons.org/licenses/by-nc-sa/3.0/
 
-# Checks the Length of 'loej' and prints: 'ok' or 'not ok'
-x = 'loej'
-
-if len(x)  > len(x):
-    print('ok')
+# A more sophisticated way to verify name and age in a database; using basic if and else statements.
+print('Welcome! Please verify your identity.')
+print('Please input your name:')
+name = input()
+print('Please input your age:')
+age = int(input())
+print('Finally, what is your password?')
+password = input()
+# The only person allowed is Ruddy the 20 year old programmer that has the password: Eggs and Bacon.
+if name == 'Ruddy' and age == 20 and password == 'Eggs and Bacon':
+    print('Welcome to your shift Ruddy! Please get to work : )')
+elif name == 'Ruddy' and age != 20 and password != 'Eggs and Bacon':
+    print('Wrong name, password OR age. Try again')
 else:
-    print('not ok')
-# Checks for name 'Joel'
-name = 'Joel'
-if(name == 'Joel'):
-    print(name)
-print('Done')
+    print('INTRUDER!')
 
-# Changes the name to 'Python', it won't print name again
-
-name = 'Python'
-if(name == 'Joel'):
-    print(name)
-print('Done')
