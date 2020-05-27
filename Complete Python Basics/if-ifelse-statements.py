8400c098e3ad888126511c2a4238d64d5880892e
loej <jfm203@scarletmail.rutgers.edu>
2020-05-27 19:33:59 -0400

    Finished if statements

---
+++ Complete Python Basics/if-ifelse-statements.py
@@ -0,0 +1,22 @@
+#[Automate the Boring Stuff with Python - Al Sweigart](http://automatetheboringstuff.com/)
+# Under the same creative commons license: https://creativecommons.org/licenses/by-nc-sa/3.0/
+
+# Checks the Length of 'loej' and prints: 'ok' or 'not ok'
+x = 'loej'
+
+if len(x)  > len(x):
+    print('ok')
+else:
+    print('not ok')
+# Checks for name 'Joel'
+name = 'Joel'
+if(name == 'Joel'):
+    print(name)
+print('Done')
+
+# Changes the name to 'Python', it won't print name again
+
+name = 'Python'
+if(name == 'Joel'):
+    print(name)
+print('Done')
