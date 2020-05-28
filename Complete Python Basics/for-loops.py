d2657effb11f0079a29ef59dc684e5209dbeadd0
loej <jfm203@scarletmail.rutgers.edu>
2020-05-27 23:15:21 -0400

    Completed while-loops excercises. Started for-loops.

---
+++ Complete Python Basics/for-loops.py
@@ -0,0 +1,8 @@
+# [Automate the Boring Stuff with Python - Al Sweigart](http://automatetheboringstuff.com/)
+# Under the same creative commons license: https://creativecommons.org/licenses/by-nc-sa/3.0/
+
+# Prints Hi 5 times, starts at 0 and advances from there. i always begins at 0.
+print('My Hi is:')
+for i in range(5):
+    print('Hi' + ' ' + str(i))
+
---
+++ Complete Python Basics/while-loops.py
@@ -0,0 +1,53 @@
+# [Automate the Boring Stuff with Python - Al Sweigart](http://automatetheboringstuff.com/)
+# Under the same creative commons license: https://creativecommons.org/licenses/by-nc-sa/3.0/
+
+# Prints 'Hello World' 5 iterations
+spam = 1
+while spam < 5:
+    print('Hello, World.')
+    spam += 1
+else:
+    print('The value is not less than 5.')
+
+# Keeps asking four string until it equals the String (Input Validation).
+name = ''
+while name != 'Joel':
+    print('Please type in your name:')
+    name = input()
+print('Thank you!')
+
+# Uses Break statement
+name = ''
+while True:
+    print('Please type your name:')
+    name = input()
+    if name == 'Joel':
+        # Immediately jumps out of loop
+        break
+print('Thanks!')
+
+# Uses Continue statement, jumps back to the while loop and reevaluates execution
+spam = 0
+while (spam < 5):
+    spam += 1
+    if (spam == 2):
+        # Immediatley jumps back to the loop and skips the print statement.
+        continue
+    print('The spam is:' + str(spam))
+
+# Printing a Star Triangle using a while loop
+triangleStars = int(input('Enter the number of rows:'))
+row = 0
+while row < triangleStars:
+    # We want each star to correspond to their index number + 1
+    star = row + 1
+    # Prints the star. Each star is printed as long as its  greater than 0. Subtracts the amount after each print.
+    while star > 0:
+        print('*', end=' ')
+        star -= 1
+    # Increments each row
+    row += 1
+    print()
+
+# Printing a heart
+
