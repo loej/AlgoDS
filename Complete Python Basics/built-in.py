931406bdf2a23d526297c4941debdad7045179ea
loej <joel.martinez@rutgers.edu>
2020-07-07 22:37:47 -0400

    Started built-in libraries lesson.

---
+++ Complete Python Basics/built-in.py
@@ -0,0 +1,18 @@
+# [Automate the Boring Stuff with Python - Al Sweigart](http://automatetheboringstuff.com/)
+# Under the same creative commons license: https://creativecommons.org/licenses/by-nc-sa/3.0/
+
+# Standard Library and modules:
+
+import random
+
+# passes a random integer between 1 and 10
+# you need the module name when calling it
+print(random.randint(1, 10))
+
+# An example of importing multiple libraries
+import sys, os, math
+
+# You can also import everything from a standard library
+from sys import *
+# The * shows it is an import of everything
+
