e63a4d4b790033108d8fbf785f74dcdbd7572fb5
loej <jfm203@scarletmail.rutgers.edu>
2020-07-27 11:52:10 -0400

    Added new LL methods.

--- Data Structures & Alogrithms/LinkedLists.py
+++ Data Structures & Alogrithms/LinkedLists.py
@@ -18,22 +18,39 @@ class LinkedList:
         while ptr:
             print(ptr.data)
             ptr = ptr.next
+    
+    def locateNode(self, x):
+        temp = self.head 
+        while (temp is not None):
+            if (temp.data == x):
+                return temp.data
+        temp = temp.next
 
     def reverseLL(self):
-       pass
-
-
-
-        
+        prev = None 
+        curr = self.head 
+        while (curr is not None):
+           next = curr.next
+           curr.next = prev 
+           prev = curr
+           curr = next
+        self.head = prev
+      
 def main():
     LLOne = LinkedList()
     LLOne.head = Node(3)
     LLtwo = Node(5)
     LLThree = Node(7)
+    LLfour = Node('hello')
 
     LLOne.head.next = LLtwo
     LLtwo.next = LLThree
+    LLThree.next = LLfour
     LLOne.loopLL()
+    LLOne.reverseLL()
+    print('--')
+    LLOne.loopLL()
+    LLOne.locateNode(3)
 
 if __name__=='__main__':
     main()
