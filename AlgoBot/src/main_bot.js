acf7629b1f46b3fec143451243615733cc0e04f3
Joel Martinez <joel.martinez@rutgers.edu>
2021-01-06 20:19:58 -0500

    Added config file

---
+++ AlgoBot/src/main_bot.js
@@ -0,0 +1,44 @@
+// requires discord.js
+const Discord = require('discord.js')
+const config = require('config.json')
+
+// client
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    console.log("Connected as " + client.user.tag)
+})
+
+client.on('message', message => {
+	console.log(message.content);
+})
+
+client.on('message', message => 
+{
+    if(message.content === '!ping'){
+        message.channel.send('pongggg')
+    }
+})
+
+client.on('message', message => {
+    if(message.content  === '!beanyyy'){
+        message.channel.send('uhhhh')
+    }
+})
+
+client.on('message', message => {
+    if(message.content  === '!Marco' || message.content  === '!marco'){
+        message.channel.send('Polo')
+    }
+})
+
+let message_arr = []
+
+client.on('message', message => {
+    message_arr.push(message.content)
+})
+
+
+//logins in as the client for algobot
+client.login(config.token)
+
