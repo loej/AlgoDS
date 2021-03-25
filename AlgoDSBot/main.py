0897135d07ffba101febc9635836a07d601013b1
loej <joel.martinez@rutgers.edu>
2021-03-25 04:19:53 -0400

    reformatted files

--- AlgoDSBot/main.py
+++ AlgoDSBot/main.py
@@ -8,5 +8,6 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
+
 client = MyClient()
 client.run()
