273b8d941d1196211c2b2bbdb9ea749f958c4d1e
loej <joel.martinez@rutgers.edu>
2021-03-25 04:53:39 -0400

    Updated bot

--- AlgoDSBot/main/main.py
+++ AlgoDSBot/main/main.py
@@ -1,4 +1,5 @@
 import discord
+import json
 
 
 class MyClient(discord.Client):
@@ -8,6 +9,16 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
+    @classmethod
+    def open_json_file(cls):
+        with open('config.json', 'r') as file:
+            data = file.read()
+        return data
 
-client = MyClient()
-client.run()
+
+if __name__ == '__main__':
+    class_data = MyClient()
+    data = class_data.open_json_file()
+    token = json.loads(data)
+    client = MyClient()
+    client.run(str(token['DISCORD_TOKEN']))
